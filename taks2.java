d71700bc10c7458406b182323fe12dd815c7d9a5
kathevpp <98862756+kathevpp@users.noreply.github.com>
2022-03-11 18:26:08 -0500

    Add files via upload

---
+++ taks2.java
@@ -0,0 +1,15 @@
+package day26_nested_loops;
+
+public class taks2 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 7; i++) {
+
+            for (int j = 1; j < 9; j++) {
+
+                System.out.print("*" + " ");
+            }
+            System.out.println("*");
+        }
+    }
+}
